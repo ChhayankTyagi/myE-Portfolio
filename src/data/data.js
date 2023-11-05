9875f4fa43d15350300d64adff9868300c35c74e
ChhayankTyagi <chhayanktyagi@gmail.com>
2023-11-05 14:05:18 +0530

    first commit

---
+++ src/data/data.js
@@ -0,0 +1,37 @@
+import miclone from '../assets/projects/miclone.jpg'
+import movix from '../assets/projects/movix.jpg'
+import portfolio from '../assets/projects/portfolio.jpg'
+import smartcare from '../assets/projects/smartcare.png'
+
+export const data=[
+    {
+        id:1,
+        name:"React JS Application",
+        image:miclone,
+        github:"",
+        live:"",
+    },
+    {
+        id:2,
+        name:"React JS Application",
+        image:movix,
+        github:"",
+        live:"",
+    },
+    {
+        id:3,
+        name:"React JS Application",
+        image:portfolio,
+        github:"",
+        live:"",
+    },
+    {
+        id:4,
+        name:"React JS Application",
+        image:smartcare,
+        github:"",
+        live:"",
+    },
+
+
+]
